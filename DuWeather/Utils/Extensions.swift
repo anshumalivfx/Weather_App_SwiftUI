3b7a63e76c7788a011ba801106fcefab884a6c27
Anshumali Karna <52081717+anshumalivfx@users.noreply.github.com>
2023-01-29 20:47:21 +0530

    Update Extensions.swift

--- DuWeather/Utils/Extensions.swift
+++ DuWeather/Utils/Extensions.swift
@@ -1,34 +1,45 @@
 //
 //  Extensions.swift
-//  DuWeather
+//  Weather
 //
-//  Created by Anshumali Karna on 25/01/23.
+//  Created by Dara To on 2022-05-30.
 //
 
 import SwiftUI
 
 extension Color {
     static let background = LinearGradient(gradient: Gradient(colors: [Color("Background 1"), Color("Background 2")]), startPoint: .topLeading, endPoint: .bottomTrailing)
-    
     static let bottomSheetBackground = LinearGradient(gradient: Gradient(colors: [Color("Background 1").opacity(0.26), Color("Background 2").opacity(0.26)]), startPoint: .topLeading, endPoint: .bottomTrailing)
-    
     static let navBarBackground = LinearGradient(gradient: Gradient(colors: [Color("Background 1").opacity(0.1), Color("Background 2").opacity(0.1)]), startPoint: .topLeading, endPoint: .bottomTrailing)
-    
-    static let tabBarColor = LinearGradient(gradient: Gradient(colors: [Color("Tab Bar Background 1").opacity(0.26), Color("Tab Bar Background 2").opacity(0.26)]), startPoint: .top, endPoint: .bottom)
-    static let weatherWidgetBackground = LinearGradient(gradient: Gradient(colors: [Color("Weather Widget Background 1"), Color("Weather Widget Background 2")]), startPoint: .top, endPoint: .bottom)
-    
+    static let tabBarBackground = LinearGradient(gradient: Gradient(colors: [Color("Tab Bar Background 1").opacity(0.26), Color("Tab Bar Background 2").opacity(0.26)]), startPoint: .top, endPoint: .bottom)
+    static let weatherWidgetBackground = LinearGradient(gradient: Gradient(colors: [Color("Weather Widget Background 1"), Color("Weather Widget Background 2")]), startPoint: .leading, endPoint: .trailing)
     static let bottomSheetBorderMiddle = LinearGradient(gradient: Gradient(stops: [.init(color: .white, location: 0), .init(color: .clear, location: 0.2)]), startPoint: .top, endPoint: .bottom)
-    
-    static let bottomSheetBorderTop = LinearGradient (gradient: Gradient(colors: [.white.opacity(0) , .white.opacity (0.5),
-                                                                                  .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
-    
-    static let underline = LinearGradient (gradient: Gradient (colors: [.white.opacity(0), .white, .white.opacity(0)]),
-                                           startPoint: .leading, endPoint: .trailing)
-    
+    static let bottomSheetBorderTop = LinearGradient(gradient: Gradient(colors: [.white.opacity(0), .white.opacity(0.5), .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
+    static let underline = LinearGradient(gradient: Gradient(colors: [.white.opacity(0), .white, .white.opacity(0)]), startPoint: .leading, endPoint: .trailing)
     static let tabBarBorder = Color("Tab Bar Border").opacity(0.5)
-    
     static let forecastCardBackground = Color("Forecast Card Background")
-    
     static let probabilityText = Color("Probability Text")
+}
+
+
+extension View {
+    func backgroundBlur(radius: CGFloat = 3, opaque: Bool = false) -> some View {
+        self
+            .background(Blur(radius: radius, opaque: opaque))
+    }
     
+    func innerShadow<S: Shape, SS: ShapeStyle>(shape: S, color: SS, line: CGFloat = 1, offsetX: CGFloat = 0, offsetY: CGFloat = 0, blur: CGFloat = 4, blendMode: BlendMode = .normal, opacity: Double = 1) -> some View {
+        return self
+            .overlay(content: {
+                shape
+                    .stroke(color, lineWidth: line)
+                    .blendMode(blendMode)
+                    .offset(x: offsetX, y: offsetY)
+                    .blur(radius: blur)
+                    .mask {
+                        shape
+                    }
+                    .opacity(opacity)
+            })
+    }
 }
